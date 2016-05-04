fc9fd99294feca4446ce8c0050db04d6efacaf6d
Maxim Orlov <mail@orlovmax.com>
2016-05-05 01:08:29 +0300

    pug data flow adjusted: added data auto loading from data directory

--- grunt_tasks/pug.js
+++ grunt_tasks/pug.js
@@ -6,9 +6,18 @@ module.exports = {
 			pretty: true,
 			data: function(dest, src) {
 				var fs = require("fs"),
-					paths = require(__dirname + '/config/paths.js');
+					paths = require(__dirname + '/config/paths.js'),
+					files = fs.readdirSync(paths.dev.data),
+					obj = {};
 
-				return { 'config': JSON.parse(fs.readFileSync(paths.dev.data + '/config.json'))};
+				for (var i in files) {
+					var key = files[i].substr(0, files[i].lastIndexOf('.')),
+						val = JSON.parse(fs.readFileSync(paths.dev.data + '/' + files[i]));
+
+					obj[key] = val;
+				}
+
+				return obj;
 			}
 		},
 		files: [ {
